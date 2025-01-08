c79cd89280820162c989ce43a0cffdf73eaf5796
Kingston <79642613+KPB98115@users.noreply.github.com>
2025-01-08 13:59:59 +0800

    Update DropdownList.jsx

--- src/components/DropdownList.jsx
+++ src/components/DropdownList.jsx
@@ -8,7 +8,7 @@ const DropdownList = (prop) => {
   return (
     <div className="dropdown_wrapper">
       <button onClick={() => setActive(!active)} className="dropdown_toggle">
-        <h2>{prop.title}</h2>
+        <h2 style={{ color: 'white' }}>{prop.title}</h2>
         <img src={active ? arrow_up : arrow_down} alt="" />
       </button>
       {active && (
